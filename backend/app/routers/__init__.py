7ca9910cdd358e7c88d4a641615c8902205a97dc
Manuel DLT <delatorrev0@gmail.com>
2026-02-28 21:18:49 -0600

    Update __init__.py

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,9 +2,5 @@
 Routers init
 """
 from . import auth
-from . import cedis
-from . import scorecards
-from . import presupuestos
-from . import documentos
 
-__all__ = ["auth", "cedis", "scorecards", "presupuestos", "documentos"]
+__all__ = ["auth"]
